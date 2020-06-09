d84338b6ea7e2622215913a98a54394ce0269ccb
Damian Olbrys <damianolbrys5@gmail.com>
2020-06-09 20:24:47 +0200

    Some changes'
    /
    as.d
    :q
    end
    asd
    a
    da
    sd
    ads
    ads
    sad
    asd
    ads
    ads
    asd
    ads
    da
    z

--- Algorithms/Sorting_Algorithms/Inesertion_Sort/Algorithm.py
+++ Algorithms/Sorting_Algorithms/Inesertion_Sort/Algorithm.py
@@ -1,1 +1,12 @@
-i = 2
+set = [2, 1, 3, 44, 6, 1, 3, 88, 21, 2, 11, 52]
+
+for j in range(1, len(set)):
+    key = set[j]
+    i = j-1
+    while i >= 0 and set[i] > key:
+        set[i+1] = set[i]
+        i = i - 1
+    set[i+1] = key
+
+
+print(set)
